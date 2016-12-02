8ab9522b288710b212db1e85479d0cb9dde57619
shareNo1 <shareNo1@shareNo1.local>
2016-12-02 14:54:09 +0900

    trying to write a graph

--- main.py
+++ main.py
@@ -13,122 +13,6 @@ import matplotlib.pyplot as plt
 flowRoot = TaskInFlow([], None, root)
 MakeTaskStFlow(flowRoot, s)
 
-'''
-print "======"
-print flowRoot.task.prob
-print flowRoot.task.lev
-print flowRoot.task.thing
-print flowRoot.depth
-print "======"
-print flowRoot.children[0].st.lev
-print flowRoot.children[0].st.thing
-print flowRoot.children[0].st.cost
-print flowRoot.children[0].depth
-print "------"
-print flowRoot.children[1].st.lev
-print flowRoot.children[1].st.thing
-print flowRoot.children[1].st.cost
-print flowRoot.children[1].depth
-print "------"
-print flowRoot.children[2].st.lev
-print flowRoot.children[2].st.thing
-print flowRoot.children[2].st.cost
-print flowRoot.children[2].depth
-print "------"
-print flowRoot.children[3].st.lev
-print flowRoot.children[3].st.thing
-print flowRoot.children[3].st.cost
-print flowRoot.children[3].depth
-print "======"
-print flowRoot.children[0].children[0].task.prob
-print flowRoot.children[0].children[0].task.lev
-print flowRoot.children[0].children[0].task.thing
-print flowRoot.children[0].children[0].depth
-print "------"
-print flowRoot.children[0].children[1].task.prob
-print flowRoot.children[0].children[1].task.lev
-print flowRoot.children[0].children[1].task.thing
-print flowRoot.children[0].children[1].depth
-print "------"
-print flowRoot.children[1].children[0].task.prob
-print flowRoot.children[1].children[0].task.lev
-print flowRoot.children[1].children[0].task.thing
-print flowRoot.children[1].children[0].depth
-print "------"
-print flowRoot.children[2].children[0].task.prob
-print flowRoot.children[2].children[0].task.lev
-print flowRoot.children[2].children[0].task.thing
-print flowRoot.children[2].children[0].depth
-print "------"
-print flowRoot.children[3].children
-print "======"
-print flowRoot.children[0].children[0].children
-print "------"
-print flowRoot.children[0].children[0].children[0].st.lev
-print flowRoot.children[0].children[0].children[0].st.thing
-print flowRoot.children[0].children[0].children[0].st.cost
-print flowRoot.children[0].children[0].children[0].depth
-print "------"
-print flowRoot.children[0].children[0].children[1].st.lev
-print flowRoot.children[0].children[0].children[1].st.thing
-print flowRoot.children[0].children[0].children[1].st.cost
-print flowRoot.children[0].children[0].children[1].depth
-print "------"
-print flowRoot.children[0].children[1].children
-print "------"
-print flowRoot.children[0].children[1].children[0].st.lev
-print flowRoot.children[0].children[1].children[0].st.thing
-print flowRoot.children[0].children[1].children[0].st.cost
-print flowRoot.children[0].children[1].children[0].depth
-print "------"
-print flowRoot.children[0].children[1].children[1].st.lev
-print flowRoot.children[0].children[1].children[1].st.thing
-print flowRoot.children[0].children[1].children[1].st.cost
-print flowRoot.children[0].children[1].children[1].depth
-print "------"
-print flowRoot.children[1].children[0].children
-print "------"
-print flowRoot.children[1].children[0].children[0].st.lev
-print flowRoot.children[1].children[0].children[0].st.thing
-print flowRoot.children[1].children[0].children[0].st.cost
-print flowRoot.children[1].children[0].children[0].depth
-print "------"
-print flowRoot.children[1].children[0].children[1].st.lev
-print flowRoot.children[1].children[0].children[1].st.thing
-print flowRoot.children[1].children[0].children[1].st.cost
-print flowRoot.children[1].children[0].children[1].depth
-print "------"
-print flowRoot.children[2].children[0].children
-print "------"
-print flowRoot.children[2].children[0].children[0].st.lev
-print flowRoot.children[2].children[0].children[0].st.thing
-print flowRoot.children[2].children[0].children[0].st.cost
-print flowRoot.children[2].children[0].children[0].depth
-print "------"
-print flowRoot.children[2].children[0].children[1].st.lev
-print flowRoot.children[2].children[0].children[1].st.thing
-print flowRoot.children[2].children[0].children[1].st.cost
-print flowRoot.children[2].children[0].children[1].depth
-print "======"
-print flowRoot.children[0].children[0].children[0].children
-print flowRoot.children[0].children[0].children[1].children
-print flowRoot.children[0].children[1].children[0].children
-print flowRoot.children[0].children[1].children[1].children
-print flowRoot.children[1].children[0].children[0].children
-print flowRoot.children[1].children[0].children[1].children
-print flowRoot.children[2].children[0].children[0].children
-print flowRoot.children[2].children[0].children[1].children
-print "======"
-print flowRoot.children[0].children[1].children[0].children[0].task.prob
-print flowRoot.children[0].children[1].children[0].children[0].task.lev
-print flowRoot.children[0].children[1].children[0].children[0].task.thing
-print flowRoot.children[0].children[1].children[0].children[0].children
-
-print flowRoot.children[0].children[1].children[0].children[0].children[0].children
-
-print flowRoot.children[0].children[1].children[0].children[0].children[1].children
-'''
-
 leaves = []
 getLeaves(flowRoot, leaves) #flow_nodeの葉ノードを集める関数	
 # leaves = [(node1), (node2), ....]
@@ -147,48 +31,12 @@ def print_flow(flow_node):
 	else:
 		return
 
-print_flow(flowRoot)
-'''
-print "======"
-print flowRoot.task.prob
-print flowRoot.task.lev
-print flowRoot.task.thing
-print flowRoot.depth
-print flowRoot.c
-print "======"
-print flowRoot.children.st.lev
-print flowRoot.children.st.thing
-print flowRoot.children.st.cost
-print flowRoot.children.depth
-print flowRoot.children.c
-print "======"
-print flowRoot.children.children
-print flowRoot.children.children[0].task.prob
-print flowRoot.children.children[0].task.lev
-print flowRoot.children.children[0].task.thing
-print flowRoot.children.children[0].depth
-print "------"
-print flowRoot.children.children[1].task.prob
-print flowRoot.children.children[1].task.lev
-print flowRoot.children.children[1].task.thing
-print flowRoot.children.children[1].depth
-print "======"
-print flowRoot.children.children[0].children
-print flowRoot.children.children[1].children
-print "------"
-print flowRoot.children.children[0].children.st.lev
-print flowRoot.children.children[0].children.st.thing
-print flowRoot.children.children[0].children.st.cost
-print flowRoot.children.children[0].children.depth
-print "------"
-print flowRoot.children.children[1].children.st.lev
-print flowRoot.children.children[1].children.st.thing
-print flowRoot.children.children[1].children.st.cost
-print flowRoot.children.children[1].children.depth
-'''
+#print_flow(flowRoot)
 
+print flowRoot.c
 #パラメータを動かした時のグラフ出力
-#x = np.arange(-3, 3, 0.1)
-#y = np.sin(x)
-#plt.plot(x, y)
-#plt.show()'''
+x = np.arange(0, 10000)
+THING_A = x
+y = flowRoot.c
+plt.plot(x, y)
+plt.show()
